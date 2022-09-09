e46c2c11624ccf454e9e670c5ca204bf75437d2f
Gabriel Amado <amadocmgabriel@gmail.com>
2022-09-09 17:02:42 -0300

    cap1ex10

---
+++ cap1/10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    char c;
+    
+    while ((c = getchar()) != EOF)
+    {
+        if (c == '\t')  
+            printf("\\t");
+
+        if (c == '\b')
+            printf("\\b");
+
+        if (c == '\\')
+            printf("\\");           
+        
+        putchar(c);
+    }   
+}
